b112cb2b4b98ec00d76c1eb924e6abd8803e5d8d
kishordevaragudi <kishordev36@gmail.com>
2023-02-02 17:19:47 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -6,23 +6,14 @@ from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
+from Insurance.components.data_transformation import DataTransformation
 
 
-#def test_logger_and_expection():
-    #try:
-    # log.info("Starting the test_logger_and_expection")
-    # result=3/0
-    # print(result)
-    # log.info("Ending point of test_logger_and_expection")
-    #except Exception as e:
-    #    log.debug(str(e))
-    #    raise InsuranceException(e, sys)
+
 
 if __name__ == "__main__":
     try:
-        #start_training_pipeline()
-        #test_logger_and_expection()
-        #get_collection_as_dataframe(database_name = "INSURANCE",collection_name ="INSURANCE_PROJECT")
+
         traning_pipeline_config = config_entity.TrainingPipelineConfig()
         data_ingestion_config = config_entity.DataIngestionConfig(traning_pipeline_config = traning_pipeline_config)
         print(data_ingestion_config.to_dict())
@@ -35,5 +26,12 @@ if __name__ == "__main__":
                                          data_ingestion_artifact = data_ingestion_artifact)
 
         data_validation_artifact = data_validation.initaite_data_validation()
+
+        # Data Transformation
+        data_transformation_config = config_entity.DataTransformationConfig(traning_pipeline_config = traning_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config = data_transformation_config, 
+        data_ingestion_artifact= data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initaite_data_transformation()
+        
     except Exception as e:
         print(e)
